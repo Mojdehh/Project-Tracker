fb2e0828dd342273273d9cb66123feb03fe0bbee
Hashim Sharif <sharif.hashim@gmail.com>
2022-01-14 19:54:58 -0500

    add seeds for db

---
+++ backend/db/seeds/01_users.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (full_name, email, password) 
+VALUES ('Alice Smith', 'asmith@gmail.com', 'password')
+INSERT INTO users (full_name, email, password) 
+VALUES ('Scott Anderson', 'anders@hotmail.com', 'password')
+INSERT INTO users (full_name, email, password) 
+VALUES ('Jane Doe', 'jdoe@ymail.com', 'password')
+INSERT INTO users (full_name, email, password) 
+VALUES ('Willam Jackson', 'willyj@yahoo.com', 'password')
