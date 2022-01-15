aedeb59f7f261167b1b42e2aabaf8ba510bb0e98
Mojdeh <mojdeh.sbzgh@gmail.com>
2022-01-15 15:40:50 -0500

    create dbHelper functions

---
+++ backend/helpers/dbHelpers.js
@@ -0,0 +1,99 @@
+module.exports = (db) => {
+  const getUsers = () => {
+      const query = {
+          text: 'SELECT * FROM users',
+      };
+
+      return db
+          .query(query)
+          .then((result) => result.rows)
+          .catch((err) => err);
+  };
+
+  const getProjects = () => {
+    const query = {
+        text: 'SELECT * FROM projects',
+    };
+
+    return db
+        .query(query)
+        .then((result) => result.rows)
+        .catch((err) => err);
+};
+
+
+  // const getUserByEmail = email => {
+
+  //     const query = {
+  //         text: `SELECT * FROM users WHERE email = $1` ,
+  //         values: [email]
+  //     }
+
+  //     return db
+  //         .query(query)
+  //         .then(result => result.rows[0])
+  //         .catch((err) => err);
+  // }
+
+  // const addUser = (firstName, lastName, email, password) => {
+  //     const query = {
+  //         text: `INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *` ,
+  //         values: [firstName, lastName, email, password]
+  //     }
+
+  //     return db.query(query)
+  //         .then(result => result.rows[0])
+  //         .catch(err => err);
+  // }
+
+  const getProjectUsers = () => {
+      const query = {
+          text: `SELECT users.full_name as name 
+          FROM users 
+          JOIN user_project ON user_id = users.id 
+          JOIN projects ON project_id = projects.id 
+          WHERE project_id = $1`,
+          values($1)
+      }
+      const values = [];
+
+      return db.query(query, values)
+          .then(result => result.rows)
+          .catch(err => err);
+
+  }
+
+  const getProjectTickets = () => {
+    const query = {
+        text: `SELECT * FROM tickets WHERE project_id = $1`,
+        values($1)
+    }
+    const values = [];
+
+    return db.query(query, values)
+        .then(result => result.rows)
+        .catch(err => err);
+
+  }
+
+  const getTicketComments = () => {
+    const query = {
+        text: ` SELECT * FROM comments WHERE ticket_id = $1`,
+        values($1)
+    }
+    const values = [];
+
+    return db.query(query, values)
+        .then(result => result.rows)
+        .catch(err => err);
+
+  }
+
+  return {
+      getUsers,
+      getProjects,
+      getProjectUsers,
+      getProjectTickets,
+      getTicketComments
+  };
+};
