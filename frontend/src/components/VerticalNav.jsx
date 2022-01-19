c1c48a75484f8a8809e887f4b236d6eabb722879
Hashim Sharif <sharif.hashim@gmail.com>
2022-01-19 10:48:47 -0500

    refactor vertical nav, for future modification

--- frontend/src/components/VerticalNav.jsx
+++ frontend/src/components/VerticalNav.jsx
@@ -1,58 +1,12 @@
 import * as React from "react";
-import PropTypes from "prop-types";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 import BasicTable from "./BasicTable";
-import TicketsTable from "./TicketsTable";
-import InteractiveList from "./InteractiveList";
-import useProjectDetail from "../hooks/useProjectsData";
 import ProjectPopUp from "./ProjectPopUp";
-import { useState } from "react";
 import useApplicationData from "../hooks/useApplicationData";
 import axios from "axios";
-import { getTableContainerUtilityClass } from "@mui/material";
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `vertical-tab-${index}`,
-    "aria-controls": `vertical-tabpanel-${index}`,
-  };
-}
 
 export default function VerticalNav() {
-  const [value, setValue] = React.useState(0);
   const [tableRow, setTableRows] = React.useState([]);
-  const projects = useProjectDetail();
   const [userId, setUserId] = React.useState([]);
-  console.log("userId", userId);
   const { state } = useApplicationData();
 
   React.useEffect(() => {
@@ -60,8 +14,6 @@ export default function VerticalNav() {
   }, [state]);
 
   function addProject(value) {
-    console.log("value:", value);
-    console.log("userID", userId);
     return axios
       .post("http://localhost:8080/api/projects", {
         projectName: value,
@@ -91,64 +43,27 @@ export default function VerticalNav() {
         }
       );
   }
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
 
   const handleClick = (value, id, event) => {
     event.preventDefault();
     addProject(value);
   };
   return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        bgcolor: "background.paper",
-        display: "flex",
-        height: 224,
-      }}
-    >
-      <Tabs
-        orientation="vertical"
-        variant="scrollable"
-        value={value}
-        onChange={handleChange}
-        aria-label="Vertical tabs example"
-        sx={{ borderRight: 1, borderColor: "divider" }}
-      >
-        <Tab label="Dashboard" {...a11yProps(0)} />
-        <Tab label="Projects" {...a11yProps(1)} />
-      </Tabs>
-      <TabPanel value={value} index={0}>
-        {/* Dashboard */}
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Projects
-        {/* <InteractiveList projects={projects} />
-        <TicketsTable
-          name="Ticket Name"
-          description="Description"
-          priority="Priority"
-          status="Status"
-          date="Date Created"
-        /> */}
-        <BasicTable
-          name="Project Name"
-          number="Number of Tickets"
-          status="Project Status"
-          date="Date Created"
-          state={tableRow}
-        />
-        <ProjectPopUp
-          name="Add a Project"
-          add="create new project"
-          userId={userId}
-          setUserId={setUserId}
-          // list={list}
-          // setList={setList}
-          handleClick={handleClick}
-        />
-      </TabPanel>
-    </Box>
+    <>
+      <BasicTable
+        name="Project Name"
+        number="Number of Tickets"
+        status="Project Status"
+        date="Date Created"
+        state={tableRow}
+      />
+      <ProjectPopUp
+        name="Add a Project"
+        add="create new project"
+        userId={userId}
+        setUserId={setUserId}
+        handleClick={handleClick}
+      />
+    </>
   );
 }
