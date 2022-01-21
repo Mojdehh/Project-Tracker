71b6a7a162d7b8448ec964eb370aeed7e7d2d7c9
Taylor Noj <taylornoj@gmail.com>
2022-01-21 11:14:53 +0000

    make comment heading larger

--- frontend/src/components/CommentSection.jsx
+++ frontend/src/components/CommentSection.jsx
@@ -7,19 +7,33 @@ import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
 import { useParams } from "react-router-dom";
+import CommentIcon from '@mui/icons-material/Comment';
+import Grid from '@mui/material/Grid';
 
 export default function CommentSection(props) {
   let { project_id, ticket_id } = useParams();
   // const comments = useCommentsData(project_id, ticket_id);
+
+  const avatarStyle = { display: 'flex', backgroundColor: '#4caf50', justifyContent: 'center', alignItems: 'center' }
 
   if (props.comments.length === 0) return "No Comments";
   console.log("props.comments", props.comments);
   return (
     
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-      <ListItem sx={{ mt: 2, mb: 2  }} variant="h6" >
-      Comments:
-        </ListItem>
+
+<Grid container direction="row" alignItems="center">
+  <Grid item>
+      <Avatar sx={{ml: 2}} style={avatarStyle}><CommentIcon /></Avatar>
+  </Grid>
+  <Grid item>
+      <Typography sx={{ mt: 2, mb: 2, ml: 1 }} variant="h6" >
+      Comments
+        </Typography>
+  </Grid>
+  </Grid>
+  <Divider variant="inset"  />
+
       {props.comments.map((row) => (
         <>
           <ListItem key={row.id} alignItems="flex-start">
