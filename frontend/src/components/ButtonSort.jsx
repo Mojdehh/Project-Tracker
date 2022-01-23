e2fb7f96c05be9e1756d8b6fa16de1684c297772
Taylor Noj <taylornoj@gmail.com>
2022-01-23 11:37:36 +0000

    writing over radio buttons follows styling of the search bar

--- frontend/src/components/ButtonSort.jsx
+++ frontend/src/components/ButtonSort.jsx
@@ -4,6 +4,7 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import Typography from '@mui/material/Typography';
 
 export default function RowRadioButtonsGroup(props) {
   const handleRadioChange = (e) => {
@@ -30,7 +31,11 @@ export default function RowRadioButtonsGroup(props) {
 
   return (
     <FormControl>
-      <FormLabel  id="demo-row-radio-buttons-group-label">Filter Projects By:</FormLabel>
+      {/* <FormLabel  id="demo-row-radio-buttons-group-label"> */}
+      <Typography variant="caption" align="left" >
+        Filter Projects By:
+        </Typography>
+        {/* </FormLabel> */}
       <RadioGroup
         row
         aria-labelledby="demo-row-radio-buttons-group-label"
