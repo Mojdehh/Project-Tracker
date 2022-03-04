c7a9dd420e5a9d5277a5b711d05157ca5a8a57a8
Mojdeh <mojdeh.sbzgh@gmail.com>
2022-03-04 16:56:16 -0500

    clean out code

--- backend/helpers/dbHelpers.js
+++ backend/helpers/dbHelpers.js
@@ -238,7 +238,6 @@ module.exports = (db) => {
       })
       .then((result) => {
         if (result !== undefined && bcrypt.compareSync(password, result.password)) {
-          console.log("user with Hashhhh", result);
           return result;
         }
         return null;
